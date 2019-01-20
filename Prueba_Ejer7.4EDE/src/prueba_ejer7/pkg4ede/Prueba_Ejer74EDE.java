7cb0d168048662fdb1b65390c996d6ed0423d9ae
JavierRiquelme <jaricer258@gmail.com>
2019-01-20 18:50:21 +0100

    Primera modificación del repositorio Prueba

---
+++ Prueba_Ejer7.4EDE/src/prueba_ejer7/pkg4ede/Prueba_Ejer74EDE.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_ejer7.pkg4ede;
+
+/**
+ *
+ * @author mac
+ */
+public class Prueba_Ejer74EDE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hola, soy el repositorio de Prueba del Ejercicio 7.4 de Entornos de Desarrollo.");
+    }
+    
+}
